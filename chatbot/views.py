d6d9462e7a0b09d7a4a8e9fda8026316a8fa09dd
Noman <80441451+nali556@users.noreply.github.com>
2023-08-26 22:13:28 -0400

    chat displaying properly

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
 # Create your views here.
 def chatbot(req):
+    if req.method == 'POST':
+        message = req.POST.get('message')
+        response = 'Hello'
+        return JsonResponse({'message': message, 'response': response})
     return render(req, 'chatbot.html')
