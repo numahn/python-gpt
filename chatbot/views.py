e1ef61137d95a2173cfde5ff5b3ff17b7085cfed
Noman <80441451+nali556@users.noreply.github.com>
2023-08-27 13:55:52 -0400

    added api, some accurate answers

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,9 +1,25 @@
 from django.shortcuts import render
 from django.http import JsonResponse
-# Create your views here.
+import openai
+
+openaiKey = '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'
+openai.api_key = openaiKey
+
+def askOpenai(message):
+    response = openai.Completion.create(
+        model = "text-davinci-003",
+        prompt = message,
+        max_tokens = 50,
+        n = 1,
+        stop = None,
+        temperature = 0.7,
+    )
+    answer = response.choices[0].text.strip()
+    return answer
+
 def chatbot(req):
     if req.method == 'POST':
         message = req.POST.get('message')
-        response = 'Hello'
+        response = askOpenai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(req, 'chatbot.html')
