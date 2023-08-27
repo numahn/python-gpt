6b984af7e360d68dec97dcd8c11064552705ad6e
Noman <80441451+nali556@users.noreply.github.com>
2023-08-27 14:20:47 -0400

    gpt integrated

--- chatbot/views.py
+++ chatbot/views.py
@@ -2,19 +2,27 @@ from django.shortcuts import render
 from django.http import JsonResponse
 import openai
 
-openaiKey = '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'
+openaiKey = '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'
 openai.api_key = openaiKey
 
 def askOpenai(message):
-    response = openai.Completion.create(
-        model = "text-davinci-003",
-        prompt = message,
-        max_tokens = 50,
-        n = 1,
-        stop = None,
-        temperature = 0.7,
+    # response = openai.Completion.create(
+    #     model = "text-davinci-003",
+    #     prompt = message,
+    #     max_tokens = 150,
+    #     n = 1,
+    #     stop = None,
+    #     temperature = 0.7,
+    # )
+    response = openai.ChatCompletion.create(
+        model = "gpt-3.5-turbo",
+        messages=[
+        {"role": "system", "content": "You are a helpful assistant that answers all questions based on your knowledge."},
+        {"role": "user", "content": message}
+    ]
     )
-    answer = response.choices[0].text.strip()
+    print(response)
+    answer = response['choices'][0]['message']['content']
     return answer
 
 def chatbot(req):
