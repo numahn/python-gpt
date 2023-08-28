8fd3be1ceea59900a27e3c7cf72f9d9d3f4e9611
Noman <80441451+nali556@users.noreply.github.com>
2023-08-27 22:47:20 -0400

    env

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,19 +1,14 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from dotenv import load_dotenv
 import openai
 
-openaiKey = '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'
+load_dotenv()
+
+
 openai.api_key = openaiKey
 
 def askOpenai(message):
-    # response = openai.Completion.create(
-    #     model = "text-davinci-003",
-    #     prompt = message,
-    #     max_tokens = 150,
-    #     n = 1,
-    #     stop = None,
-    #     temperature = 0.7,
-    # )
     response = openai.ChatCompletion.create(
         model = "gpt-3.5-turbo",
         messages=[
