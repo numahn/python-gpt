ec217849a3c770f1a0cf5c354c17b6bd5f7d8b75
Noman <80441451+nali556@users.noreply.github.com>
2023-08-28 17:09:21 -0400

    register done

--- chatbot/views.py
+++ chatbot/views.py
@@ -1,9 +1,10 @@
 from dotenv import load_dotenv
 import os
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.http import JsonResponse
 import openai
-
+from django.contrib import auth
+from django.contrib.auth.models import User
 load_dotenv()
 
 openai.api_key = os.environ.get("API_KEY")
@@ -26,3 +27,31 @@ def chatbot(req):
         response = askOpenai(message)
         return JsonResponse({'message': message, 'response': response})
     return render(req, 'chatbot.html')
+
+def login(req):
+    
+    return render(req, 'login.html')
+
+def register(req):
+    if req.method == 'POST':
+        username = req.POST['username']
+        email = req.POST['email']
+        password1 = req.POST['password1']
+        password2 = req.POST['password2']
+        if password1 == password2:
+            try:
+                user = User.objects.create_user(username, email, password1)
+                user.save()
+                auth.login(req, user)
+                return redirect('chatbot')
+            except:
+                errormsg = "Error creating the account"
+                return render(req, 'register.html', {'error_message': errormsg}) 
+
+        else:
+            errormsg = "Passwords do not match"
+            return render(req, 'register.html', {'error_message': errormsg}) 
+    return render(req, 'register.html')
+
+def logout(req):
+    auth.logout(req)
